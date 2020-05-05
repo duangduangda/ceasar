2432d5f59753043c509bbf962360b45686e9a45e
yaohua.dong <yaohua.dong@nio.com>
2020-05-05 18:02:07 +0800

    change file path

---
+++ src/main/java/org/dean/caesar/CaesarApplication.java
@@ -0,0 +1,13 @@
+package org.dean.caesar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaesarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaesarApplication.class, args);
+    }
+
+}
